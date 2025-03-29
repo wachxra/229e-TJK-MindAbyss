3715f911c1def0f205f4a00141629d62d5af81da
wachxra <wachirawadee.junt@bumail.net>
2025-03-30 04:09:12 +0700

    Added BG music and sound click to menu UI
    - Can added Audio Source to MainMenu, both EndGame and EndCredit
    - But in the EndCredit scene is unfinished version so I will added to EndCredit scene soon

--- Assets/Scripts/Finished/MainMenu.cs
+++ Assets/Scripts/Finished/MainMenu.cs
@@ -5,18 +5,45 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Sounds Settings")]
+    public AudioSource backgroundMusic;
+    public AudioSource clickSound;
+
+    void Start()
+    {
+        if (backgroundMusic != null && !backgroundMusic.isPlaying)
+        {
+            backgroundMusic.loop = true;
+            backgroundMusic.Play();
+        }
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene("TestingScene");
+        StartCoroutine(PlayButtonClickAndLoadScene("TestingScene"));
     }
 
     public void QuitGame()
     {
+        if (clickSound != null)
+        {
+            clickSound.Play();
+        }
         Application.Quit();
     }
 
     public void EndCredit()
     {
-        SceneManager.LoadScene("EndCredit");
+        StartCoroutine(PlayButtonClickAndLoadScene("EndCredit"));
+    }
+
+    private IEnumerator PlayButtonClickAndLoadScene(string sceneName)
+    {
+        if (clickSound != null)
+        {
+            clickSound.Play();
+            yield return new WaitForSeconds(clickSound.clip.length);
+        }
+        SceneManager.LoadScene(sceneName);
     }
 }
